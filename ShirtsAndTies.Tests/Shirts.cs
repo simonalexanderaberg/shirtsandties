31ba48e87771d4ee91b57647d696a87d141b56e7
SIMONNN <simplonn@hotmail.com>
2016-11-11 11:55:16 +0100

    Revert "Revert "init""
    This reverts commit f5a1bb0a1aa7691e23ea9312ca756b13cfdda5ee.

---
+++ ShirtsAndTies.Tests/Shirts.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using ShirtsAndTies.Lib;
+
+namespace ShirtsAndTies.Tests
+{
+    [TestClass]
+    public class Shirts
+    {
+        [TestMethod]
+        public void Test_ShirtSalesProcessor()
+        {
+            SaleProcessorFactory factory = new SaleProcessorFactory();
+            ISalesProcessor salesprocessor = factory.CreateSaleProcessor(ProductTypeEnum.PTEnum.Shirt);
+            ShirtsAndTiesFacade facade = new ShirtsAndTiesFacade(salesprocessor);
+            facade.Get("i love ny");
+            salesprocessor.Process("i love ny");
+            Assert.IsNotNull(salesprocessor);
+            Assert.IsNotNull(facade);
+        }
+    }
+}
